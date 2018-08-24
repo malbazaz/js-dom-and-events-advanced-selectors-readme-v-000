c6192d11309a834877e4472e225012dd633569d2
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-24 21:31:40 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+$(document).ready(function paragraphSelector(){
+  return $('p')
+});
+$(document).ready(function lastImageSelector(){
+  return $('img:last')
+});
+$(document).ready(function ninjaBabySelector(){
+  return $('p')
+});
+$(document).ready(function paragraphSelector(){
+  return $('p')
+});
+$(document).ready(function paragraphSelector(){
+  return $('p')
+});
